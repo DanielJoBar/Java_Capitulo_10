1f999abc3d3da89f4fda6e30b2ac732f759716bc
DanielJoBar <115071984+DanielJoBar@users.noreply.github.com>
2023-02-13 23:40:09 +0100

    Add files via upload

---
+++ Ejercicio08/Ejercicio08.java
@@ -0,0 +1,24 @@
+package Ejercicio08;
+import java.util.ArrayList;
+public class Ejercicio08 {
+  public static void main(String[] args) {
+    
+    ArrayList<Carta> c = new ArrayList<Carta>();
+    
+    Carta cartaAux = new Carta();
+    c.add(cartaAux);
+    
+    for (int i = 0; i < 9; i++) {
+      do {
+        cartaAux = new Carta();      
+      } while (c.contains(cartaAux));
+      
+      c.add(cartaAux);
+    }
+      
+    for (Carta miCarta: c) {
+      System.out.println(miCarta);
+    }
+  }
+}
+
